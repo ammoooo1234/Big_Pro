856f1f297a13c4a0c57681d864cf369bd25c1b94
ammoooo1234 <56770251+ammoooo1234@users.noreply.github.com>
2022-08-23 16:16:33 +0530

    Input from user

--- project 2 random story.py
+++ project 2 random story.py
@@ -1,14 +1,13 @@
 import random
-Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time']
-character = [' there lived a king.',' there was a man named Jack.',
+Sentence_starter =[item for item in input("Enter the list items : ").split()] #['About 100 years ago', ' In the 20 BC', 'Once upon a time']
+character =[item for item in input("Enter the list items : ").split()] #[' there lived a king.',' there was a man named Jack.',
 			' there lived a farmer.']
-time = [' One day', ' One full-moon night']
-story_plot = [' he was passing by',' he was going for a picnic to ']
-place = [' the mountains', ' the garden']
-second_character = [' he saw a man', ' he saw a young lady']
-age = [' who seemed to be in late 20s', ' who seemed very old and feeble']
-work = [' searching something.', ' digging a well on roadside.']
-lst2 = [item for item in input("Enter the list items : ").split()]
+time =[item for item in input("Enter the list items : ").split()] #[' One day', ' One full-moon night']
+story_plot =[item for item in input("Enter the list items : ").split()] #[' he was passing by',' he was going for a picnic to ']
+place =[item for item in input("Enter the list items : ").split()] #[' the mountains', ' the garden']
+second_character =[item for item in input("Enter the list items : ").split()] #[' he saw a man', ' he saw a young lady']
+age =[item for item in input("Enter the list items : ").split()] #[' who seemed to be in late 20s', ' who seemed very old and feeble']
+work =[item for item in input("Enter the list items : ").split()] #[' searching something.', ' digging a well on roadside.']
 
 # Selecting an item from each list and concatenating them.
 print(random.choice(Sentence_starter)+random.choice(character)+
